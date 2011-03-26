227e9ed8832b2ecbd18ce270b786d250934f82fe
ruslan ulanov <ruslan.ulanov@gmail.com>
2011-03-25 17:07:20 -0700

    Moved comments to Wiki

--- config.js
+++ config.js
@@ -1,53 +1,44 @@
+/*
+* This is the main configuration file. It contains set-up instructions 
+* for your panels.
+* Please carefully read documentation before editing this file.
+*/
 var CONFIG = {
 	
-	isOffline: true, 
-		/*
-		// Set to 'false' before uploading to ISY.
-		// Set to 'true' for development. 
-		// When set to 'true':
-		// -logging window will be enabled automatically
-		// -placeholder images will be used instead of actual camera JPEGs
-		// -no commands will be sent to ISY, but will be logged
-		*/
-	showLog: true,
-	enableLog: true,
-		/*
-		// Show debug/log area
-		*/
-	name: "My Home", 
-		/*
-		// Name for your set-up. It will appear on the first button in the GUI
-		*/
-	zipcode: '94588',
-		/*
-		// Five digit US zipcode for weather forecast
-		*/
+	IsOffline: true, 
+
+	ShowLog: true,
+
+	EnableLog: true,
+
+	Name: "My Home", 
+
+	ZipCode: '94588',
 	
-	panels: 
+	Panels: 
 	[
 		{ 
-			addr:  '192.168.20.27', 	// a list of ip addresses to which this panel will be shown
-			admin: true,				// allow to show advanced controls?
-			name:  'Kitchen',			// name of the panel
-			css: './style02.css',		// CSS file for this panel
-			webcams:					// an array of web/ip camera configurations
+			addr:  '#kitchen,192.168.20.27',
+			admin: true,
+			name:  'Kitchen',
+			css: './style02.css',
+			webcams:		
 			[
 				{ id: "cam_fr", name: "Family Room", addr: "http://192.168.20.4", imgUrl: "/SnapshotJPEG?Resolution=320x240&Quality=Clarity" },
 				{ id: "cam_gr", name: "Guest Room", addr: "http://192.168.20.2:8002", imgUrl: "/SnapshotJPEG?Resolution=320x240&Quality=Clarity" }
 			],
-			areas: 					// locations (rooms or other logical groups of controls)
+			areas:
 			[
-				{ 
+				{
 					name: 'Kitchen / Dining',
-					controls: 
+					controls:
 					[
 						{ name: "Overhead", addr: '10024', type: 'scene' }
-						//{ name: "Fan", addr: '00.00.00 1', type: 'device' }
 					]
 				},
 				{
 					name: "Living room",
-					controls: 
+					controls:
 					[
 						{ name: "Stairs", addr: '10008', type: 'scene' },
 						{ name: "Track lights", addr: '10007', type: 'scene' },
@@ -63,19 +54,20 @@ var CONFIG = {
 						{ name: "Good Night", addr: '10020', type: 'scene' },
 						{ name: "All Off", addr: '00:21:b9:01:02:92', type: 'global' }
 					]
-				} //end of area
-			] // end of areas
+				}
+			]
 		},
 		{
 			addr: '*',
 			admin: false,
 			name: "Mi Casa",
 			css: './style01.css',
-			areas: 
+			areas:
 			[
 				{
 					name: "Living room",
-					controls: [
+					controls: 
+					[
 						{ name: "Stairs", addr: '10008', type: 'scene' },
 						{ name: "Track lights", addr: '10007', type: 'scene' },
 						{ name: "Bar", addr: '32988', type: 'scene' },
@@ -84,7 +76,8 @@ var CONFIG = {
 				},
 				{
 					name: "Master Bedroom",
-					controls: [
+					controls: 
+					[
 						{ name: "Her lights", addr: 'A.DF.C8 1', type: 'device' },
 						{ name: "His lights", addr: 'A.DF.C8 3', type: 'device' },
 						{ name: "Floor lamp", addr: 'A.DF.C8 2', type: 'device' },
@@ -93,7 +86,8 @@ var CONFIG = {
 				},
 				{
 					name: "Scenes",
-					controls: [
+					controls: 
+					[
 						{ name: "Party Mode", addr: '5.14.82 1', type: 'device' },
 						{ name: "Good Morning", addr: '00.00.00 1', type: 'scene' },
 						{ name: "Good Night", addr: '10020', type: 'scene' },
@@ -102,12 +96,12 @@ var CONFIG = {
 				},
 				{ 
 					name: "Kitchen",
-					controls: [
+					controls:
+					[
 						{ name: "Overhead", addr: '10024', type: 'scene' }
-						//{ name: "Music", addr: '00.00.00 1', type: 'device' }
 					]
-				} //end of area
-			] // end of areas
-		} // end of panel
-	] // end of panels
-}; // end of CONFIG
+				}
+			]
+		}
+	]
+};
